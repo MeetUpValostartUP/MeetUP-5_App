649f5674d2be43284f1ccbfa302fcdb1abe55194
Vladimir Gutorov <vlgutv@gmail.com>
2017-01-25 13:22:44 +0200

    Get route params

--- app/routes/task-list.js
+++ app/routes/task-list.js
@@ -1,7 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model() {
+    // beforeModel: function(params){
+    // //    this.set('query_id', transition.queryParams.query_id);
+    //    console.log(params.query_id);
+    // },
+    model(params) {
+        var query_id = params.query_id;
       return Ember.RSVP.hash({
           users: this.store.findAll('user'),
           tasks: this.store.findAll('task')
