36c8db9a7c75a9bfd7fc467d2759d4f764789b02
Vladimir Gutorov <vlgutv@gmail.com>
2017-01-25 14:36:10 +0200

    Change route model

--- app/routes/task-list.js
+++ app/routes/task-list.js
@@ -1,15 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    // beforeModel: function(params){
-    // //    this.set('query_id', transition.queryParams.query_id);
-    //    console.log(params.query_id);
-    // },
-    model(params) {
-        var query_id = params.query_id;
-      return Ember.RSVP.hash({
-          users: this.store.findAll('user'),
-          tasks: this.store.findAll('task')
-      });
-    }
+  // beforeModel: function(params){
+  // //    this.set('query_id', transition.queryParams.query_id);
+  //    console.log(params.query_id);
+  // },
+  model(params) {
+    var query_id = params.query_id,
+        _this = this;
+    return Ember.RSVP.hash({
+        user: _this.store.findRecord('user', query_id),
+        id: query_id
+    });
+  }
 });
