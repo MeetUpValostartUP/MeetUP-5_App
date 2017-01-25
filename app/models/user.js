a88312258604ca19f2e958c8fd8bce1a7c57023c
Vladimir Gutorov <vlgutv@gmail.com>
2017-01-25 12:49:17 +0200

    Define model in route

--- app/models/user.js
+++ app/models/user.js
@@ -9,6 +9,6 @@ const {
 export default Model.extend({
   name: attr('string'),
   password: attr('string'),
-  avatar: attr('string')
+  avatar: attr('string'),
   tasks: hasMany('task', { async: true })
 });
--- app/routes/task-list.js
+++ app/routes/task-list.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    model() {
+      return Ember.RSVP.hash({
+          users: this.store.findAll('user'),
+          tasks: this.store.findAll('task')
+      });
+    }
 });
