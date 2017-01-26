f97038027cd11d35679c5656905d10a12dd37118
Vladimir Gutorov <vlgutv@gmail.com>
2017-01-26 11:06:55 +0200

    Add functional for sorting tasks More info about sorting:   - Method sort: http://emberjs.com/api/classes/Ember.computed.html#method_sort   - Method computed: http://emberjs.com/api/classes/Ember.computed.html

--- app/components/task-list.js
+++ app/components/task-list.js
@@ -1,5 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
+  byKeyword: '',
+  tasks_filtered: Ember.computed('tasks', 'byKeyword', function() {
+    var keyword = this.get('byKeyword');
+    var filtered = this.get('tasks');
+    if (keyword.length > 0) {
+      keyword = keyword.toLowerCase().trim();
+      filtered = this.get('tasks').filter((item) => item.get('title').toLowerCase().includes(keyword));
+    }
+    return filtered;
+    }),
+  todosSortingDesc: ['deadline:desc'],
+  sortedTasks: Ember.computed.sort('tasks_filtered', 'todosSortingDesc'),
+  actions: {
+    sortDir(dir) {
+        console.log(dir);
+        this.set('todosSortingDesc', ['deadline:'+ dir +'']);
+    }
+  }
 });
