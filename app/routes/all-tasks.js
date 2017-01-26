57ef258797681d591006740d6d1a42f952bcba79
Vladimir Gutorov <vlgutv@gmail.com>
2017-01-26 11:36:20 +0200

    Add new model for users

--- app/routes/all-tasks.js
+++ app/routes/all-tasks.js
@@ -3,7 +3,8 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model() {
       return Ember.RSVP.hash({
-          tasks: this.store.findAll('task')
+          tasks: this.store.findAll('task'),
+          users: this.store.findAll('user')
       });
     }
 });
