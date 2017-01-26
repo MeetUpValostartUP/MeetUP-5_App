d498a6ee140f1c5f7c632d8b061bfb50d38b4116
Vladimir Gutorov <vlgutv@gmail.com>
2017-01-26 12:03:52 +0200

    Add functional for sorting by autor

--- app/components/task-list-all.js
+++ app/components/task-list-all.js
@@ -2,21 +2,30 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   byKeyword: '',
-  tasks_filtered: Ember.computed('tasks', 'byKeyword', function() {
-    var keyword = this.get('byKeyword');
-    var filtered = this.get('tasks');
-    if (keyword.length > 0) {
-      keyword = keyword.toLowerCase().trim();
+  byUserName: '',
+  tasks_filtered: Ember.computed('tasks', 'byKeyword', 'byUserName', function() {
+    var keyword = this.get('byKeyword').toLowerCase(),
+      userName = this.get('byUserName'),
+      filtered = this.get('tasks');
+    console.log(userName);
+    keyword = keyword.toLowerCase().trim();
+    if (userName.length > 0) {
+      filtered = this.get('tasks').filter((item) => item.get('title').toLowerCase().includes(keyword)
+                                                 && item.get('autor.name') === userName);
+    } else {
       filtered = this.get('tasks').filter((item) => item.get('title').toLowerCase().includes(keyword));
     }
     return filtered;
-    }),
+  }),
   todosSortingDesc: ['deadline:desc'],
   sortedTasks: Ember.computed.sort('tasks_filtered', 'todosSortingDesc'),
   actions: {
     sortDir(dir) {
-        console.log(dir);
-        this.set('todosSortingDesc', ['deadline:'+ dir +'']);
+      console.log(dir);
+      this.set('todosSortingDesc', ['deadline:' + dir + '']);
+    },
+    userSelect(user) {
+      this.set('byUserName', user);
     }
   }
 });
